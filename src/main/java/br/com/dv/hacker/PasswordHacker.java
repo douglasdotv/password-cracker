4e59ed0a94b764043be295e5dd51d8f0258bac2e
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-28 19:06:18 -0300

    Handle invalid port numbers, add error messages

--- src/main/java/br/com/dv/hacker/PasswordHacker.java
+++ src/main/java/br/com/dv/hacker/PasswordHacker.java
@@ -7,11 +7,22 @@ import java.net.Socket;
 public class PasswordHacker {
     public static void main(String[] args) {
         if (args.length != 3) {
+            System.out.println("Invalid number of arguments.");
             return;
         }
 
         String host = args[0];
-        int port = Integer.parseInt(args[1]);
+        int port;
+        try {
+            port = Integer.parseInt(args[1]);
+            if (port < 0 || port > 65535) {
+                System.out.println("Invalid port number.");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number.");
+            return;
+        }
         String message = args[2];
 
         try (Socket socket = new Socket(host, port);
@@ -22,7 +33,7 @@ public class PasswordHacker {
             String response = input.readUTF();
             System.out.println(response);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("An error occurred while connecting to the server: " + e.getMessage());
         }
     }
 }
