5eaec7f827bf1f8f4b0020ceddc70e2e7fb0c825
Douglas Vieira <douglasdotv@hotmail.com>
2023-04-29 11:30:01 -0300

    Implement brute force algorithm to try passwords

--- src/main/java/br/com/dv/hacker/PasswordHacker.java
+++ src/main/java/br/com/dv/hacker/PasswordHacker.java
@@ -5,13 +5,14 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 
 public class PasswordHacker {
+
     public static void main(String[] args) {
-        if (args.length != 3) {
+        if (args.length != 2) {
             System.out.println("Invalid number of arguments.");
             return;
         }
 
-        String host = args[0];
+        String ipAddress = args[0];
         int port;
         try {
             port = Integer.parseInt(args[1]);
@@ -23,17 +24,54 @@ public class PasswordHacker {
             System.out.println("Invalid port number.");
             return;
         }
-        String message = args[2];
 
-        try (Socket socket = new Socket(host, port);
-             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-             DataInputStream input = new DataInputStream(socket.getInputStream())
-        ) {
-            output.writeUTF(message);
-            String response = input.readUTF();
-            System.out.println(response);
+        String password = bruteForcePassword(ipAddress, port);
+        System.out.println(password);
+    }
+
+    private static String bruteForcePassword(String ipAddress, int port) {
+        String currentPassword = "";
+        String response = "";
+
+        try (Socket socket = new Socket(ipAddress, port);
+             DataInputStream input = new DataInputStream(socket.getInputStream());
+             DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+            while (!response.contains("Connection success!")) {
+                currentPassword = generateNextPassword(currentPassword);
+                output.writeUTF(currentPassword);
+                output.flush();
+                response = input.readUTF();
+            }
         } catch (Exception e) {
             System.out.println("An error occurred while connecting to the server: " + e.getMessage());
+            e.printStackTrace();
         }
+
+        return currentPassword;
+    }
+
+    private static String generateNextPassword(String currentPassword) {
+        StringBuilder newPassword = new StringBuilder(currentPassword);
+        int index = newPassword.length() - 1;
+
+        while (index >= 0) {
+            char currentChar = newPassword.charAt(index);
+            if (currentChar == 'z') {
+                newPassword.setCharAt(index, '0');
+                break;
+            } else if (currentChar == '9') {
+                newPassword.setCharAt(index, 'a');
+                index--;
+            } else {
+                newPassword.setCharAt(index, (char) (currentChar + 1));
+                break;
+            }
+        }
+
+        if (index < 0) {
+            newPassword.insert(0, 'a');
+        }
+
+        return newPassword.toString();
     }
 }
